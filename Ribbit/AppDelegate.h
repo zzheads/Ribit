b59a59529341dc00e6c5fd149adec1089db76866
Alexey Papin <zzheads@gmail.com>
2016-12-28 14:31:46 +0300

    .

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Ben Jakuben on 7/29/13.
+//  Copyright (c) 2013 Treehouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
